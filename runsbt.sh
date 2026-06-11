450e1392f5c1ea4f7a25b52ab73ad26164934afa
Tuure Savuoja <tuure@tuuresavuoja.net>
2026-06-12 00:57:16 +0300

    runsbt.sh: download sbt launcher, update sbt to 1.12.11

--- runsbt.sh
+++ runsbt.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
 BASEDIR=$(dirname "$0")
+RUNSBT_DIR="$BASEDIR/.runsbt"
+LAUNCHER_JAR="$RUNSBT_DIR/sbt-launch.jar"
+SBTVER="1.12.11"
 
-java -jar "$BASEDIR/.runsbt/sbt-launch.jar" "$@"
+# download sbt-launch.jar if it doesn't exist
+if [ ! -f "$LAUNCHER_JAR" ]; then
+  echo "Downloading sbt-launch-$SBTVER.jar..."
+  mkdir -p "$RUNSBT_DIR"
+  curl -f -L -o "$LAUNCHER_JAR" \
+    "https://repo1.maven.org/maven2/org/scala-sbt/sbt-launch/$SBTVER/sbt-launch-$SBTVER.jar" \
+    || { echo "Failed to download sbt-launch.jar"; exit 1; }
+  echo "Download complete."
+fi
+
+java -jar "$LAUNCHER_JAR" "$@"
